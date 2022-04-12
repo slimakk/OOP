9a09f6bae50a2ef95e9830087583740f5eb8d377
Slimakk <miroslav.babela.jr@gmail.com>
2022-04-12 09:56:42 +0200

    CV10

---
+++ CV10/CV10/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CV10")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CV10")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dcf9a5f4-419a-4f20-bd2c-7c37d3f00991")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CV10/CV10/SQLQuery3.sql
@@ -0,0 +1,16 @@
+﻿SELECT * FROM [Student] LEFT JOIN
+[StudentPredmet] on Id = StudentKluc
+LEFT JOIN [Predmet] on Skratka = PredmetKluc;
+SELECT Priezvisko, COUNT (Priezvisko) as Pocet
+FROM [Student]
+GROUP BY Priezvisko
+ORDER BY Pocet DESC;
+SELECT PredmetKluc FROM [StudentPredmet] GROUP BY PredmetKluc
+HAVING COUNT(StudentKluc) < 3;
+SELECT PredmetKluc, 
+COUNT(StudentKluc) as Pocet, 
+MIN(Hodnotenie) as Najlepsie,
+MAX(Hodnotenie) as Najhorsie,
+AVG(Hodnotenie) as Priemerne
+FROM [Hodnotenie]
+GROUP BY PredmetKluc;
